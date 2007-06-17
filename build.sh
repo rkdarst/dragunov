afa8c5ceb8c25b7964a515f73630d490d14b3ffe
rkd <rkd@zgib.net>
2007-06-17 22:42:43 +0300

    update build script with -Wall
    darcs-hash:20070617194243-51e1e-a41fcb2cec68d8259e056ef547418886377eaac6.gz

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-gcc -O2 -shared -fPIC -c dragunov_c.c
-gcc -O2 -shared -fPIC dragunov_c.o -o dragunov_c.so
+gcc -Wall -O2 -shared -fPIC -c dragunov_c.c
+gcc -Wall -O2 -shared -fPIC dragunov_c.o -o dragunov_c.so
 
 # With icc:
 #icc -O2 -shared -fPIC -c dragunov_c.c
